569136a132087a04201dcdf3ab83987e8b5de6c7
drivanreis <dr.ivanreis@gmail.com>
2024-07-20 17:39:20 -0300

    Adcionando o ngrok no CORS

--- backend/src/middlewares/cors.ts
+++ backend/src/middlewares/cors.ts
@@ -16,6 +16,7 @@ const accessControl: express.RequestHandler = (_req, res, next) => {
   res.header('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,OPTIONS,PUT,PATCH');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('ngrok-skip-browser-warnings', 'true');
   next();
 };
 
